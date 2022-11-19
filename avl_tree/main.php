aaa731932acf1ec6a15b12cf7e6293ea5f6965ef
SavageBoyz <84143@bk.ru>
2022-11-19 12:25:26 +0300

    Work with a subtree height values

--- avl_tree/main.php
+++ avl_tree/main.php
@@ -3,16 +3,20 @@
 class Node
 {
     private int $value;
+    private int $leftSubtreeHeight = 0;
+    private int $rightSubtreeHeight = 0;
     private ?Node $parent;
     private ?Node $leftChild;
     private ?Node $rightChild;
 
-    public function __construct($value, $parent = null, $leftChild = null, $rightChild = null)
+    public function __construct($value, $parent = null, $leftChild = null, $rightChild = null, $leftSubtreeHeight = 0, $rightSubtreeHeight = 0)
     {
         $this->value = $value;
         $this->parent = $parent;
         $this->leftChild = $leftChild;
         $this->rightChild = $rightChild;
+        $this->leftSubtreeHeight = $leftSubtreeHeight;
+        $this->rightSubtreeHeight = $rightSubtreeHeight;
     }
 
     /**
@@ -29,6 +33,38 @@ class Node
     public function setValue(int $value): void
     {
         $this->value = $value;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLeftSubtreeHeight(): int
+    {
+        return $this->leftSubtreeHeight;
+    }
+
+    /**
+     * @param int $leftSubtreeHeight
+     */
+    public function setLeftSubtreeHeight(int $leftSubtreeHeight): void
+    {
+        $this->leftSubtreeHeight = $leftSubtreeHeight;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRightSubtreeHeight(): int
+    {
+        return $this->rightSubtreeHeight;
+    }
+
+    /**
+     * @param int $rightSubtreeHeight
+     */
+    public function setRightSubtreeHeight(int $rightSubtreeHeight): void
+    {
+        $this->rightSubtreeHeight = $rightSubtreeHeight;
     }
 
     /**
@@ -80,7 +116,7 @@ class Node
     }
 }
 
-abstract class Result
+abstract class Response
 {
     private bool $isError;
     private string $messageText;
@@ -125,7 +161,7 @@ abstract class Result
 
 }
 
-class GetNodeByValueResult extends Result
+class GetNodeByValueResponse extends Response
 {
     private Node $result;
 
@@ -174,6 +210,7 @@ class AVLTree
             $this->rootNode = new Node($value, null);
             return;
         }
+
         $nodeSearchResult = $this->getNodeByValue($value, $this->rootNode);
         if (!$nodeSearchResult->isError()) {
             throw new ErrorException('Node with a given value is exist already.');
@@ -184,42 +221,66 @@ class AVLTree
 
         if ($foundNode->getValue() > $value) {
             $foundNode->setLeftChild($newNode);
+            $foundNode->setLeftSubtreeHeight($foundNode->getLeftSubtreeHeight() + 1);
         } else {
             $foundNode->setRightChild($newNode);
+            $foundNode->setRightSubtreeHeight($foundNode->getRightSubtreeHeight() + 1);
         }
+        $this->recursiveUpdateSubtreeHeight($foundNode);
     }
 
     /**
      * @throws ErrorException
      */
-    private function getNodeByValue(int $value, Node $currentNode): GetNodeByValueResult
+    private function getNodeByValue(int $value, Node $currentNode): GetNodeByValueResponse
     {
         if ($currentNode->getValue() === $value) {
-            return new GetNodeByValueResult($currentNode);
+            return new GetNodeByValueResponse($currentNode);
         }
 
         if ($currentNode->getValue() > $value) {
             if ($currentNode->getLeftChild() === null) {
-                return new GetNodeByValueResult($currentNode, true, 'Node is not exist.');
+                return new GetNodeByValueResponse($currentNode, true, 'Node is not exist.');
             }
             return $this->getNodeByValue($value, $currentNode->getLeftChild());
         }
 
         if ($currentNode->getValue() < $value) {
             if ($currentNode->getRightChild() === null) {
-                return new GetNodeByValueResult($currentNode, true, 'Node is not exist.');
+                return new GetNodeByValueResponse($currentNode, true, 'Node is not exist.');
             }
             return $this->getNodeByValue($value, $currentNode->getRightChild());
         }
 
         throw new ErrorException('Something went wrong while looking up a node.');
     }
+
+    private function recursiveUpdateSubtreeHeight(Node $node): void
+    {
+        $nodeParent = $node->getParent();
+
+        if ($nodeParent === null) {
+            return;
+        }
+
+        //TODO: учесть, что при удалении будет $nodeParent->getLeftSubtreeHeight() - 1 и $nodeParent->getRightSubtreeHeight() - 1
+        if ($nodeParent->getValue() > $node->getValue()) {
+            $nodeParent->setLeftSubtreeHeight($nodeParent->getLeftSubtreeHeight() + 1);
+        } else {
+            $nodeParent->setRightSubtreeHeight($nodeParent->getRightSubtreeHeight() + 1);
+        }
+        $this->recursiveUpdateSubtreeHeight($nodeParent);
+    }
 }
 
-$AVLTree = new AVLTree();
-$AVLTree->insert(5);
-$AVLTree->insert(2);
-$AVLTree->insert(8);
-$AVLTree->insert(3);
-$AVLTree->insert(15);
-$foo;
+try {
+    $AVLTree = new AVLTree();
+    $AVLTree->insert(5);
+    $AVLTree->insert(2);
+    $AVLTree->insert(8);
+    $AVLTree->insert(3);
+    $AVLTree->insert(15);
+    $foo;
+} catch(ErrorException $e) {
+    var_dump($e);
+}
